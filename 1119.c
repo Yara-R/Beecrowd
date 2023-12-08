5aaf9f00128582edf0e08178caff4251f7fbf289
Yara Rodrigues Inácio <yara.rodrigues.inacio@gmail.com>
2023-12-08 13:29:58 -0300

    Update 1119.c

--- 1119.c
+++ 1119.c
@@ -2,107 +2,145 @@
 #include <stdlib.h>
 
 typedef struct Node {
-    int valor;
-    struct Node* prox;
-    struct Node* ant;
+    int id;
+    struct Node* prev;
+    struct Node* next;
+    int removido;
 } Node;
 
-Node* createNode(int valor) {
-    Node* novo = (Node*)malloc(sizeof(Node));
+void inicializar_lista(Node** head, Node** tail, int n);
+void marcar_como_removido(Node** head, Node** tail, Node* node);
+Node* proximo_nao_removido(Node* node, int direcao);
+int tamanho_nao_removido(Node* head, Node* tail);
 
-    novo->valor = valor;
-    novo->prox = NULL;
-    novo->ant = NULL;
+int main(int argc, char** argv) {
 
-    return novo;
-}
+    int n, k, m;
 
-int main() {
-    int N, k, m;
+    while (scanf("%d %d %d", &n, &k, &m), n) {
 
-    while (1) {
-        scanf("%d %d %d", &N, &k, &m);
+        Node* head = NULL;
+        Node* tail = NULL;
 
-        if (N == 0 && k == 0 && m == 0) {
-            break;
-        }
+        inicializar_lista(&head, &tail, n);
 
-        Node* head = createNode(1);
-        Node* tail = head;
+        Node* K = head;
+        Node* M = tail;
 
-        for (int i = 2; i <= N; i++) {
+        while (tamanho_nao_removido(head, tail) > 2) {
+            
+            K = proximo_nao_removido(K, 1);
+            M = proximo_nao_removido(M, 0);
 
-            tail->prox = createNode(i);
-            tail->prox->ant = tail;
-            tail = tail->prox;
-        }
-
-        tail->prox = head;
-        head->ant = tail;
-
-        Node* sentidoHorario = head;
-        Node* sentidoAntiHorario = tail;
-
-        int removedCount = 0;
-
-        while (removedCount < N) {
-            for (int i = 1; i < k; i++) {
-
-                sentidoHorario = sentidoHorario->prox;
+            for (int i = 1; i < k; ++i) {
+                K = proximo_nao_removido(K, 1);
             }
 
-            for (int i = 1; i < m; i++) {
-
-                sentidoAntiHorario = sentidoAntiHorario->ant;
+            for (int i = 1; i < m; ++i) {
+                M = proximo_nao_removido(M, 0);
             }
 
-            if (sentidoHorario == sentidoAntiHorario) {
-
-                printf("%3d", sentidoHorario->valor);
-                removedCount++;
-            } 
-            else {
-
-                printf("%3d%3d", sentidoHorario->valor, sentidoAntiHorario->valor);
-                removedCount += 2;
-            }
-
-            if (removedCount < N) {
-                printf(",");
-            }
-
-            Node* tempHorario = sentidoHorario;
-            Node* tempAntiHorario = sentidoAntiHorario;
-
-            sentidoHorario = sentidoHorario->prox;
-            sentidoAntiHorario = sentidoAntiHorario->ant;
-
-            tempHorario->ant->prox = tempHorario->prox;
-            tempHorario->prox->ant = tempHorario->ant;
-
-            free(tempHorario);
-
-            if (tempHorario != tempAntiHorario) {
-
-                tempAntiHorario->ant->prox = tempAntiHorario->prox;
-                tempAntiHorario->prox->ant = tempAntiHorario->ant;
-                
-                free(tempAntiHorario);
+            if (K == M) {
+                printf("%3d,", K->id);
+                marcar_como_removido(&head, &tail, K);
+            } else {
+                printf("%3d%3d,", K->id, M->id);
+                marcar_como_removido(&head, &tail, K);
+                marcar_como_removido(&head, &tail, M);
             }
         }
 
-        printf("\n");
+        if (tamanho_nao_removido(head, tail) == 2) {
+            K = proximo_nao_removido(K, 1);
+            M = proximo_nao_removido(M, 0);
 
-        Node* current = head;
+            if (K == M) {
+                printf("%3d,%3d\n", K->id, K->next->id);
+            } else {
+                printf("%3d%3d\n", K->id, M->id);
+            }
+        } else {
+            printf("%3d\n", head->id);
+        }
 
-        do {
-            Node* prox = current->prox;
-
-            free(current);
-            current = prox;
-
-        } while (current != head);
+        free(head);
     }
 
     return 0;
+}
+
+void inicializar_lista(Node** head, Node** tail, int n) {
+
+    *head = (Node*)malloc(sizeof(Node));
+
+    *tail = *head;
+    (*head)->id = 1;
+
+    (*head)->prev = NULL;
+    (*head)->next = NULL;
+
+    (*head)->removido = 0;
+
+    for (int i = 2; i <= n; ++i) {
+
+        Node* novo_no = (Node*)malloc(sizeof(Node));
+
+        novo_no->id = i;
+        novo_no->prev = *tail;
+        novo_no->next = NULL;
+        novo_no->removido = 0;
+
+        (*tail)->next = novo_no;
+        *tail = novo_no;
+    }
+
+    (*head)->prev = *tail;
+    (*tail)->next = *head;
+}
+
+void marcar_como_removido(Node** head, Node** tail, Node* node) {
+    node->removido = 1;
+
+    while ((*head)->removido) {
+        *head = (*head)->next;
+    }
+
+    while ((*tail)->removido) {
+        *tail = (*tail)->prev;
+    }
+}
+
+Node* proximo_nao_removido(Node* node, int direcao) {
+
+    if (direcao == 0) {
+        node = node->prev;
+
+        while (node->removido) {
+            node = node->prev;
+        }
+    } else {
+        node = node->next;
+
+        while (node->removido) {
+            node = node->next;
+        }
+    }
+
+    return node;
+}
+
+int tamanho_nao_removido(Node* head, Node* tail) {
+
+    int count = 0;
+    Node* current = head;
+
+    do {
+        if (!current->removido) {
+            count++;
+        }
+
+        current = current->next;
+    } while (current != head);
+
+    return count;
 }
